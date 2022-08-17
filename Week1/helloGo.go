a16c315217675e20361bf0e2da0e338344d7992a
Austin P Blaise <pbaustin96@gmail.com>
2022-08-17 09:32:38 -0400

    removing commented line

--- Week1/helloGo.go
+++ Week1/helloGo.go
@@ -3,13 +3,7 @@ package main
 
 import "fmt"
 
-// func main() {
-//  fmt.Println("\n\nHello Go\n\n")
-
-// }
 func main() {
-	s := make([]int, 0, 3)
-	s = append(s, 100)
-	fmt.Println(len(s), cap(s))
-  }
-  
+	fmt.Println("\n\nHello Go\n\n")
+
+}
